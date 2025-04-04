c8bdc87a9a0cfeda57754ef23a18c37f49db8e5d
rrahulkumaryadav347 <rrahulkumaryadav347@gmail.com>
2025-04-04 17:06:28 +0530

    navbar added

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+
+    images: {
+        domains: ['img.freepik.com'], // Add this line
+      },
+};
 
 export default nextConfig;
